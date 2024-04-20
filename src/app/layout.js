9b002d0aac00d8a5c05d6fb1a9e243bfd65d3c7d
Amadeus <amadeuskang@gmail.com>
2024-04-20 14:39:22 -0400

    Update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <body
         className={`${inter.className} bg-gradient-to-br from-gray-900 to-slate-900`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,19 @@
 import Image from 'next/image';
 import Personal from './Personal';
 import Resume from './Resume';
+import Layout from './layout'; // adjust the path based on your project structure
 // import Projects from './Projects';
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start mb-2 pt-12 md:space-x-48 mx-8">
-        <Personal />
-        <Resume />
-        {/* <Projects/> */}
-      </div>
-    </main>
+    <Layout>
+      <main>
+        <div className="flex flex-col items-center md:flex-row md:justify-center md:items-start mb-2 pt-12 md:space-x-48 mx-8">
+          <Personal />
+          <Resume />
+          {/* <Projects/> */}
+        </div>
+      </main>
+    </Layout>
   );
 }
