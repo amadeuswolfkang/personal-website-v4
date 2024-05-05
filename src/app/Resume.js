f3a794a6d1589759a1fbf4e0834683975fee1447
Amadeus <amadeuskang@gmail.com>
2024-05-05 18:25:46 -0400

    Fix responsive style from adding project cards

--- src/app/Resume.js
+++ src/app/Resume.js
@@ -95,7 +95,7 @@ const Resume = () => {
         <a
           href="https://leagueranker.netlify.app/"
           target="_blank"
-          class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          class="flex flex-col items-center max-w-md bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         >
           <Image
             class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg p-4"
@@ -135,7 +135,7 @@ const Resume = () => {
         <a
           href="/nuke-stats-d3-bounce.gif"
           target="_blank"
-          class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          class="flex flex-col max-w-md items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         >
           <Image
             class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg p-4"
